4286855bcd9e4f002f7d37f1410375625d7964e6
yomogi07dra-lu <yomogi.07.dra@gmail.com>
2025-11-29 14:33:04 +0900

    POSTリクエスト・バリデーション・プレスホルダーの追加

--- dekitayoproject/app/forms.py
+++ dekitayoproject/app/forms.py
@@ -6,10 +6,32 @@ class UsersModelForm(forms.ModelForm):
     
     class Meta:
         model = Users
-        fields = ["username", "email", "password"]
+        fields = ["email", "password","username"]
+        labels = {
+            'email': 'メールアドレス',
+            'password': 'パスワード',
+            'username': 'ユーザー名',
+        }
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'test@test.com'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': '8文字以上、英字と数字を組み合わせてください'
+            }),
+            'username': forms.TextInput(attrs={
+                'placeholder': 'はなこ'
+            }),
+        }
+
+        help_texts = {
+            "username": "",
+        }
 
 class Family_membersModelForm(forms.ModelForm):
 
     class Meta:
         model = Family_members
         fields = ('role',)
+        widgets = { 'role': forms.RadioSelect }
+
--- dekitayoproject/app/views.py
+++ dekitayoproject/app/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 from . import forms
 
 def signup(request):
-    form = forms.UsersModelForm()
+    User_form = forms.UsersModelForm()
+    Family_members_form = forms.Family_membersModelForm()
+
+    if request.method == 'POST':
+        User_form = forms.UsersModelForm(request.POST)
+        Family_members_form = forms.Family_membersModelForm(request.POST)
+        if User_form.is_valid() and Family_members_form.is_valid():
+            User_form.save()
+            Family_members_form.save()
+            return redirect('home')
+
     return render(
         request, 
         'app/signup.html', 
         context={
-            'signup': form
+            'signup': User_form,
+            'role': Family_members_form,
         }
     )
