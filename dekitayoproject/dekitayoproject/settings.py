3d7c02059ead529f6ee8c112ee0c963be4110238
yomogi07dra-lu <yomogi.07.dra@gmail.com>
2026-02-15 14:25:04 +0900

    設定　修正

--- dekitayoproject/dekitayoproject/settings.py
+++ dekitayoproject/dekitayoproject/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-jh2nx=#fxw033@2p1aa-*xuwo38sax-!y0%4$5vot0@xmbl%+@
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "192.168.3.16",
+
 ]
 
 
